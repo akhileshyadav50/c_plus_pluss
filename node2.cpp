033c4c00f7d4054d954594aa3caa403981feb1a9
Akhilesh Yadav <akhilyadav9554@gmail.com>
2023-02-03 19:41:54 +0530

    node2

---
+++ node2.cpp
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+#include <iostream>
+
+using namespace std;
+
+struct node {
+  int data;
+  struct node *left;
+  struct node *right;
+};
+
+// New node creation
+struct node *newNode(int data) {
+  struct node *node = (struct node *)malloc(sizeof(struct node));
+
+  node->data = data;
+
+  node->left = NULL;
+  node->right = NULL;
+  return (node);
+}
+
+// Traverse Preorder
+void traversePreOrder(struct node *temp) {
+  if (temp != NULL) {
+    cout << " " << temp->data;
+    traversePreOrder(temp->left);
+    traversePreOrder(temp->right);
+  }
+}
